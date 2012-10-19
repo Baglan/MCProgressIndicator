9a65a6bcb058a1256a13b627c5a5f8980d70a2a4
Baglan Dosmagambetov <baglan.dos@gmail.com>
2012-10-19 14:33:57 +0600

    Initial Commit

---
+++ MCProgressIndicator/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  MCProgressIndicator
+//
+//  Created by Baglan on 10/19/12.
+//  Copyright (c) 2012 MobileCreators. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
---
+++ MCProgressIndicator/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  MCProgressIndicator
+//
+//  Created by Baglan on 10/19/12.
+//  Copyright (c) 2012 MobileCreators. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
